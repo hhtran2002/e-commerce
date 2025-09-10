b11b060fd7ed03730273d34f0cc9a5713f1b216b
hhtran2002 <huatrancm123@gmail.com>
2025-09-10 12:01:40 +0700

    cấu trúc file

---
+++ frontend/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello Ecommerce 🚀</h1>
+      <p>Vite + React + TypeScript đã chạy thành công!</p>
+    </div>
+  );
+}
+
+export default App;
